43b388affe538aeef95a87859f2979e27d073c5f
Raumaan Ahmed <raumaanahmed2310@gmail.com>
2021-09-11 02:59:02 +0100

    So it begins...

---
+++ GameOfLife.java
@@ -0,0 +1,13 @@
+public class GameOfLife {
+	private int board_width;
+	private int board_height;
+	private boolean[][] board;
+
+	public GameOfLife(){
+		boardwidth = 0;
+		boardheight = 0;
+		board = new int[board_width][board_height];
+	}
+
+	
+}
