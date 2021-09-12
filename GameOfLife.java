0aafa221a2b839e5517c8be1f35c25101fed71ed
Raumaan Ahmed <raumaanahmed2310@gmail.com>
2021-09-12 21:20:46 +0100

    Implemented Next

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,32 +1,32 @@
 public class GameOfLife {
-	public String[][] MainBoard;
+	private int[][] MainBoard;
 
 	public GameOfLife(int rows, int columns) {
 		MainBoard = RandomState(rows, columns);
 	}
 
-	public String[][] DeadState(int rows, int columns) {
-		String[][] DeadBoard = new String[rows][columns];
+	public int[][] DeadState(int rows, int columns) {
+		int[][] DeadBoard = new int[rows][columns];
 
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				DeadBoard[i][j] = " ";
+				DeadBoard[i][j] = 0;
 			}
 		}
 
 		return DeadBoard;
 	}
 
- 	public String[][] RandomState(int rows, int columns) {
-		String[][] RandomBoard = new String[rows][columns];
+ 	public int[][] RandomState(int rows, int columns) {
+		int[][] RandomBoard = new int[rows][columns];
 		double RandomNumber = 0;
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				RandomNumber = Math.random();
 				if (RandomNumber >= 0.5) {
-					RandomBoard[i][j] = "ඞ";
+					RandomBoard[i][j] = 1;
 				} else {
-					RandomBoard[i][j] = " ";
+					RandomBoard[i][j] = 0;
 				}
 				
 			}
@@ -35,7 +35,7 @@ public class GameOfLife {
 		return RandomBoard;
 	}
 
-	public void RenderState(String[][] board) {
+	public void RenderState(int[][] board) {
 		int columns = board[0].length;
         int rows = board.length;
         
@@ -47,13 +47,46 @@ public class GameOfLife {
         }
 	}
 	
-	public String[][] getBoard() {
+	public int[][] getBoard() {
 		return MainBoard;
 	}
 
-	public void NextState(String[][] board) {
-		String[][] NextBoardState = board;
+	public int[][] NextState(int[][] InitialBoardState) {
+		int columns = InitialBoardState[0].length;
+		int rows = InitialBoardState.length;
+		int[][] NextBoardState = new int[rows][columns];
+
+		for (int x = 0; x < rows; x++ ) {
+			for (int y = 0; y < columns; y++) {
+				int neighbours = 0;
+				for (int i = (x - 1) % (rows + 1); i < (x + 1) % (rows + 1); i++) {
+					for (int j = (y - 1) % (columns + 1); j < (y + 1) % (columns + 1); j++) {
+						if (InitialBoardState[i][j] == 1) {
+							neighbours++;
+						}
+					} 
+				}
+
+				if (InitialBoardState[x][y] == 1) {
+					if (neighbours == 2 | neighbours == 3) {
+						NextBoardState[x][y] = 1;
+					} else {
+						NextBoardState[x][y] = 0;
+					}
+
+				} else if (InitialBoardState[x][y] == 0) {
+					if (neighbours == 3) {
+						NextBoardState[x][y] = 1;
+					} else {
+						NextBoardState[x][y] = 0;
+					}
+				}
+			} 
+		}
+
+		return NextBoardState;
 		
+
 	}
 
 }
