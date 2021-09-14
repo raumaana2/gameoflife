c70d31918dfa19c6970c2aea13ee935d5e7e2e16
Raumaan Ahmed <87394739+arrrayyy@users.noreply.github.com>
2021-09-14 17:27:15 +0100

    Please just work thanks

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class GameOfLifeLoop {
     private GameOfLifeStates states;
     private int[][] MainBoard;
@@ -25,32 +24,5 @@ public class GameOfLife {
 
         test.InfiniteLoop();
     }
-=======
-class GameOfLifeLoop {
-    private GameOfLifeStates states;
-    private int[][] MainBoard;
-    public GameOfLifeLoop(int rows, int columns) {
-        states = new GameOfLifeStates();
 
-        MainBoard = states.RandomState(rows, columns);
-
-
-
-         
-    }
-
-    public void InfiniteLoop() {
-        while(true) {
-            states.RenderState(states.NextState(MainBoard));
-        }
-    }
-}
-
-public class GameOfLife {
-    public static void main(String[] args) {
-        GameOfLifeLoop test = new GameOfLifeLoop(1920, 1080);
-
-        test.InfiniteLoop();
-    }
->>>>>>> 9399a178de4bae193034b63a6602c7a0911702a0
 }
